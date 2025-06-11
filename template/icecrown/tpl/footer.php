d1f216c2d2c762e0c318f7dfc46ae21b93f77d82
Hilcer Noa <hnoa@tesabiz.com>
2025-06-11 09:15:47 -0400

    Actualizacion inicial del proyecto

---
+++ template/icecrown/tpl/footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author Amin Mahmoudi (MasterkinG)
+ * @copyright    Copyright (c) 2019 - 2024, MasterkinG32 (https://masterking32.com)
+ * @link    https://masterking32.com
+ **/
+?>
+<div class="row">
+    <div class="text-center box1">
+        Siguenos en Facebook <a href="https://www.facebook.com/Plan3kGaming">Facebook.com</a>
+        - <?php echo "Load " . (new SebastianBergmann\Timer\ResourceUsageFormatter)->resourceUsageSinceStartOfRequest(); ?>
+    </div>
+</div>
+</div>
+</div>
+</body>
+</html>
---
+++ template/icecrown/tpl/rules.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by Amin.MasterkinG
+ * Website : MasterkinG32.CoM
+ * Email : lichwow_masterking@yahoo.com
+ * Date: 11/26/2018 - 8:36 PM
+ */
+?>
+
+<p>
+    <?php elang('read_before_register'); ?>
+</p>
+<ul>
+    <li>Trampas, uso de bots, modificaciones y programas de terceros, abuso de errores y exploits</li>
+    <li>Multiboxing</li>
+    <li>Compartir cuentas, carreras GDKP, aumentos, venta de oro y otros tipos de RMT</li>
+    <li>Interacciones con el personal</li>
+    <li>Cosas a tener en cuenta en un servidor de rol como Turtle WoW</li>
+    <li>Reglas de nombres para un servidor de rol como Turtle WoW</li>
+    <li>Abuso en el chat</li>
+    <li>Comportamiento durante encuentros PvP</li>
+    <li>Acoso</li>
+    <li>Informes de comportamiento</li>
+    <li>Solicitar asistencia de GM para misiones con errores</li>
+    <li>Problemas de distribución de botín</li>
+    <li>Restauración de objetos</li>
+    <li>Reembolsos de tokens</li>
+    <li>Apelación de decisiones</li>
+    <li>Descargo de responsabilidad</li>
+</ul>
